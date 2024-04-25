1963de1720d1a4bbe9942b3fab4c41d7b47e51a0
charles <charles.braquet@e.email>
2024-04-25 22:00:24 +0200

    generate stl

--- make_gear.js
+++ make_gear.js
@@ -21,9 +21,9 @@ function getCmd(dName, fName) {
 	const rCmd = [];
 	//rCmd.push('pwd');
 	//rCmd.push(`ls refs/${dName}`);
-	rCmd.push(`npx designix-cli -d=gear/${desiName} -o=refs/${dName} --outFileName=px_${fName}.json write json_param`);
-	//rCmd.push(`npx designix-cli -d=gear/${desiName} -p=refs/${dName}/px_${fName}.json -o=refs/${dName} --outFileName=${fName}.scad write scad_3d_openscad`);
-	//rCmd.push(`openscad -o refs/${dName}/${fName}.stl refs/${dName}/${fName}.scad`);
+	//rCmd.push(`npx designix-cli -d=gear/${desiName} -o=refs/${dName} --outFileName=px_${fName}.json write json_param`);
+	rCmd.push(`npx designix-cli -d=gear/${desiName} -p=refs/${dName}/px_${fName}.json -o=refs/${dName} --outFileName=${fName}.scad write scad_3d_openscad`);
+	rCmd.push(`openscad -o refs/${dName}/${fName}.stl refs/${dName}/${fName}.scad`);
 	//rCmd.push(`npx shx rm -fr refs/${dName}`);
 	return rCmd
 }
